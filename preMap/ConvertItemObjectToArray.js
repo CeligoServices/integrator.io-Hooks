f126220c92645cdf7bbc3b21e355350bbd5ada1c
Christopher Snelson <clsnelson@macbook-pro-2.lan>
2019-07-17 09:05:40 -0700

    updated comment

--- preMap/ConvertItemObjectToArray.js
+++ preMap/ConvertItemObjectToArray.js
@@ -1,5 +1,8 @@
+/**
+This PreMap integrator.io hook will check a certain property in the data coming into the import and convert it to an array if it isn't already an array.
+*/
 function convertObjToArray(options) {
-  var elementName = 'item';
+  var elementName = 'item';  //Change item to be the name of the property that holds the items
   for(var i=0; i<options.data.length; i++){
     if(Array.isArray(options.data[i][elementName]))
       continue;
